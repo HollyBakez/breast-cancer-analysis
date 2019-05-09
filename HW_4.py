0b84db4450be807b73168878a1d39e82fa50e90d
HollyBakez <ho.holland1999@gmail.com>
2019-05-08 19:13:24 -0700

    removed bad data

--- HW_4.py
+++ HW_4.py
@@ -7,8 +7,16 @@ import sqlite3
 
 # read the data set as dataFrame
 data = pd.read_csv('breast-cancer-wisconsin.csv')
+
+#drops incomplete rows
+indexQuestion = data[ data['bare_nucleoli'] == '?'].index
+data.drop(indexQuestion, inplace = True)
+data.bare_nucleoli = data.bare_nucleoli.astype(int)
 # dropping id to only get the columns with values needed
 data_no_id = data.drop(['id'], axis = 1)
+
+print(data_no_id.bare_nucleoli.unique)
+
 # transfering the contents of the data into SQL data base
 conn = sqlite3.connect('breast_cancer_analysis.db')
 data.to_sql('breast', con = conn, if_exists = 'replace')
@@ -26,25 +34,11 @@ df_json = pd.read_json('breast.json', orient = 'split')
 def findMean_STD(column_Choice):
     col_mean = data[column_Choice].mean()
     col_std = data[column_Choice].std()
-    return print("The", column_Choice, "Mean is:", col_mean, "\nThe", column_Choice,"STD is: ", col_std)
-    #return print("The",column_Choice,"mean is:",data[column_Choice].mean(), "\nThe", column_Choice, "STD is: ", data[column_Choice].std())
+    print("The", column_Choice, "Mean is:", col_mean, "\nThe", column_Choice,"STD is: ", col_std)
 
 print(type(data))
-#print(df_db)
 print(df_json)
+
 # displaying the mean and std of every column
-'''
-findMean_STD('clump_thickness')
-findMean_STD('size_uniformity')
-findMean_STD('shape_uniformity')
-findMean_STD('marginal_adhesion')
-findMean_STD('epithelial_size')
-findMean_STD('bare_nucleoli')
-findMean_STD('bland_chromatin')
-findMean_STD('normal_nucleoli')
-findMean_STD('mitoses')
-findMean_STD('class')
-'''
 for index in data_no_id.columns:
-    print(index)
     findMean_STD(index)
